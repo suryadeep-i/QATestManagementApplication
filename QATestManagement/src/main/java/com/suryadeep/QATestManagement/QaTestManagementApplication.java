27b44271ebeda3fb712e7a827b49d254ffd6774f
surya <suryadeep.ivaturi@gmail.com>
2026-04-02 12:57:44 +0530

    First commit

---
+++ QATestManagement/src/main/java/com/suryadeep/QATestManagement/QaTestManagementApplication.java
@@ -0,0 +1,13 @@
+package com.suryadeep.QATestManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaTestManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaTestManagementApplication.class, args);
+	}
+
+}
---
+++ QATestManagement/src/test/java/com/suryadeep/QATestManagement/QaTestManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suryadeep.QATestManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaTestManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
